ddea1b39f5fbecdbd18345c478cd547b213cf25b
mart-gant <martgant@gmail.com>
2024-11-24 12:31:01 +0100

    Adding class Database

---
+++ zadanie1/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database
+{
+    private PDO $pdo;
+
+    public function __construct(string $dsn, string $user, string $password)
+    {
+        try {
+            $this->pdo = new PDO($dsn, $user, $password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    private function getPdo(): PDO
+    {
+        return $this->pdo;
+    }
+}
